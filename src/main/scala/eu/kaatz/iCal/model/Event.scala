7dc2792a03430135fe797e3b222ccf163e2bd26c
unknown <kaatz@sf007.FRIENDS>
2012-03-15 14:04:41 +0100

    GMT

--- src/main/scala/eu/kaatz/iCal/model/Event.scala
+++ src/main/scala/eu/kaatz/iCal/model/Event.scala
@@ -82,7 +82,7 @@ trait EventImplicits {
     append(e.description).
     append("')").toString) ~
     ("tags", JArray(e.tags.map(x => JString(x.trim)).toList)) ~
-    ("start", e.start.toString()) ~
-    ("end", e.end.toString())
+    ("start", e.start.toGMTString()) ~
+    ("end", e.end.toGMTString())
   implicit def EList2Json(l: List[Event]) = JArray(l map { Event2Json(_) })
 }
